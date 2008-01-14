ce64315e5c739c832f1f0af79f7e8cd7a163efc9
Ben Wallberg <wallberg@umd.edu>
2008-01-14 19:46:24 +0000

    extract dates for testing normalization
    git-svn-id: https://svn.lib.umd.edu/fedora/trunk/ead@460 63312a1b-192c-0410-8ca7-9abd9f405716

--- test/src/org/mith/ead/data/TestDateHandler.java
+++ test/src/org/mith/ead/data/TestDateHandler.java
@@ -1,6 +1,8 @@
 package org.mith.ead.data;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -32,18 +34,52 @@ public class TestDateHandler {
   static Logger log = Logger.getLogger(TestDateHandler.class.getName());
 
   /**
-	* Extract all dates from the db.
-	*/
+   * Extract all dates from the db.
+   */
 
-  public static void extract(DataConvertor dc) {
+  public static void extract(DataConvertor dc) throws Exception {
+    OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(new File("dates.txt")));
+
+    Statement st = dc.conn.createStatement();
+
+    System.out.println("Extracting dates from BoxList");
+    String query = "SELECT date FROM BoxList";
+                
+    log.debug(query);
+    ResultSet rs = st.executeQuery(query) ;         
+
+    while (rs.next()) {
+      String strDate = rs.getString(1);
+      w.write(strDate + "\n");
+    }
+
+    System.out.println("Extracting dates from ItemList");
+    query = "SELECT date FROM ItemList";
+                
+    log.debug(query);
+    rs = st.executeQuery(query) ;         
+
+    while (rs.next()) {
+      String strDate = rs.getString(1);
+      w.write(strDate + "\n");
+    }
+    rs.close();
+
+    w.close();
+    st.close();
   }
 
-  public static void main(String args[]) throws Exception {
-	 DatabaseProperty dp = new DatabaseProperty();
-	 dp.reset();
-	 DataConvertor dc = new DataConvertor(dp);
-	 dc.connect();
 
-	 extract(dc);
+  /**
+   * Command line interface.
+   */
+
+  public static void main(String args[]) throws Exception {
+    DatabaseProperty dp = new DatabaseProperty();
+    dp.reset();
+    DataConvertor dc = new DataConvertor(dp);
+    dc.connect();
+
+    extract(dc);
   }
 }
